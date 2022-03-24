b6a619073de8a1bdf0bb7f9eee27b8b30004af6c
Ryszawy <71441335+Ryszawy@users.noreply.github.com>
2022-03-24 17:57:32 +0100

    Projekt Etap 0

---
+++ Projekt_Wsp/Calculator/Calculator.cs
@@ -0,0 +1,25 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        public double AddNumbers(double x, double y) {
+            return x + y;
+        }
+
+        public double SubtractionNumbers(double x, double y) {
+            return x - y; 
+        }
+
+        public double MultiplicationNumbers(double x, double y) {
+            return x * y;
+        }
+
+        public double DivisionNumbers(double x, double y) {
+
+            if (y == 0) {
+                throw new ArgumentException("Nie można dzielić przez zero");
+            }
+            return x / y;
+        }
+    }
+}
